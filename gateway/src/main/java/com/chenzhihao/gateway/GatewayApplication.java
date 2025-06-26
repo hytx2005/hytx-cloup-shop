bb4f4348f658d16fc31263d25bd08ab6bbf0b275
S9059774 <Aa15918108861@>
2025-06-26 15:49:23 +0800

    feat:改造成springboot项目

--- gateway/src/main/java/com/chenzhihao/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/chenzhihao/gateway/GatewayApplication.java
@@ -1,11 +1,8 @@
 package com.chenzhihao.gateway;
 
-import com.alibaba.cloud.nacos.annotation.NacosConfig;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class GatewayApplication implements InitializingBean{
