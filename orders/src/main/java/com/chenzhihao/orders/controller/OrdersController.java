257fd7140834da76ce8ad8caf8f009116b3aa0c7
yang <2332359987@qq.com>
2025-06-29 20:28:25 +0800

    6 订单表的增删改查

--- orders/src/main/java/com/chenzhihao/orders/controller/OrdersController.java
+++ orders/src/main/java/com/chenzhihao/orders/controller/OrdersController.java
@@ -1,9 +1,12 @@
 package com.chenzhihao.orders.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.chenzhihao.orders.domain.po.Orders;
+import com.chenzhihao.orders.service.IOrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +19,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrdersController {
+    @Autowired
+    private IOrdersService ordersService;
+
+    /**
+     * 添加订单
+     * @param orders 订单对象
+     * @return 添加结果
+     */
+    @PostMapping
+    public boolean addOrder(@RequestBody Orders orders) {
+        return ordersService.save(orders);
+    }
+
+    /**
+     * 删除订单
+     * @param id 订单ID
+     * @return 删除结果
+     */
+    @DeleteMapping("/{id}")
+    public boolean deleteOrder(@PathVariable Long id) {
+        return ordersService.removeById(id);
+    }
+
+    /**
+     * 修改订单
+     * @param orders 订单对象
+     * @return 修改结果
+     */
+    @PutMapping
+    public boolean updateOrder(@RequestBody Orders orders) {
+        return ordersService.updateById(orders);
+    }
+
+    /**
+     * 查询订单列表
+     * @return 订单列表
+     */
+    @GetMapping
+    public List<Orders> getOrders() {
+        return ordersService.list();
+    }
+
+    /**
+     * 根据ID查询订单
+     * @param id 订单ID
+     * @return 订单对象
+     */
+    @GetMapping("/{id}")
+    public Orders getOrderById(@PathVariable Long id) {
+        return ordersService.getById(id);
+    }
 
 }
