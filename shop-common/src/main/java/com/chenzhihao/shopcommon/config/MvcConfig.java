9e052e68310f087b56b8ba3eb57b931964c2f707
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2025-07-02 20:35:56 +0800

    shop-common模块新增userInfo拦截器，用来从请求头中获取到对应的userId 配置userInfo拦截器拦截路径为/**

---
+++ shop-common/src/main/java/com/chenzhihao/shopcommon/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.chenzhihao.shopcommon.config;
+
+import com.chenzhihao.shopcommon.interceptor.UserInfoInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnClass(DispatcherServlet.class)
+public class MvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private UserInfoInterceptor userInfoInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInfoInterceptor);
+    }
+}
