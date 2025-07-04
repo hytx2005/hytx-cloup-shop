7fe27da71f86a7e59541ff78053e594a62c6ec10
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2025-07-04 16:25:49 +0800

    创建生成订单号相关的dto、vo实体类

---
+++ orders/src/main/java/com/chenzhihao/orders/domain/dto/OrderDetailDto.java
@@ -0,0 +1,19 @@
+package com.chenzhihao.orders.domain.dto;
+
+/**
+ * 单个订单详情表
+ * @author dhx
+ */
+public class OrderDetailDto {
+
+
+    /**
+     * 商品id
+     */
+    private Long commodityId;
+
+    /**
+     * 商品数量
+     */
+    private Integer num;
+}
---
+++ orders/src/main/java/com/chenzhihao/orders/domain/vo/OrderCreVo.java
@@ -0,0 +1,13 @@
+package com.chenzhihao.orders.domain.vo;
+
+/**
+ * 生成订单返回实体类
+ * @author dhx
+ */
+public class OrderCreVo {
+
+    /**
+     * 订单号
+     */
+    private String orderNo;
+}
