f3649111330f5d8762713d3dbb9d2f0ee2616831
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2025-07-04 22:28:10 +0800

    修改aop，重新导入依赖使其生效

--- shop-common/src/main/java/com/chenzhihao/shopcommon/advice/DubboAop.java
+++ shop-common/src/main/java/com/chenzhihao/shopcommon/advice/DubboAop.java
@@ -1,12 +1,13 @@
 package com.chenzhihao.shopcommon.advice;
 
-import com.chenzhihao.shopcommon.annotation.DubboServiceAop;
+import com.chenzhihao.shopcommon.annotation.DubboException;
 import com.chenzhihao.shopcommon.result.Result;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +17,7 @@ import java.lang.reflect.Method;
 @Component
 public class DubboAop {
 
-    @Pointcut("@annotation(com.chenzhihao.shopcommon.annotation.DubboServiceAop)")
+    @Pointcut("@annotation(com.chenzhihao.shopcommon.annotation.DubboException)")
     public void dubboService() {
 
     }
@@ -30,7 +31,7 @@ public class DubboAop {
             return Result.success(result);
         }
         catch (RuntimeException e){
-            DubboServiceAop dubboServiceAop = method.getAnnotation(DubboServiceAop.class);
+            DubboException dubboServiceAop = method.getAnnotation(DubboException.class);
             if (dubboServiceAop != null) {
                 // 获取 message 属性值
                 String message = dubboServiceAop.message();
