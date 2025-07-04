ff9fd46e017879e0cb7a0f264eaed8e01ea920be
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2025-07-04 12:29:49 +0800

    修改获取购物车服务，获取商品信息的同时更新购物车内容

--- carts/src/main/java/com/chenzhihao/carts/mapper/CartMapper.java
+++ carts/src/main/java/com/chenzhihao/carts/mapper/CartMapper.java
@@ -2,6 +2,8 @@ package com.chenzhihao.carts.mapper;
 
 import com.chenzhihao.carts.domain.po.Cart;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chenzhihao.carts.domain.vo.CartVO;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
 
 /**
@@ -16,4 +18,14 @@ public interface CartMapper extends BaseMapper<Cart> {
 
     @Update("UPDATE cart SET commodity_num = commodity_num + 1 WHERE commodity_id = #{commodityId} AND user_id = #{userId}")
     boolean updateNum(Long commodityId, Long userId);
+
+
+    /**
+     * 更新购物车表的内容
+     * @param vo
+     */
+    @Update("update cart set commodity_name = #{vo.commodityName}, commodity_price = #{vo.commodityPrice}, " +
+            "spec = #{vo.spec}, commodity_url = #{vo.commodityUrl} " +
+            "where commodity_id = #{vo.commodityId} and user_id = #{vo.userId} and id = #{vo.id}")
+    void updateCartByCommodity(@Param("vo") CartVO vo);
 }
